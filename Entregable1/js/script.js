8689c4640925de03b751df4b00f5980f3d8e0ae0
Rubén García Pedrejón <62309228+rubengp39@users.noreply.github.com>
2021-10-14 14:41:00 +0200

    script js searchbar

---
+++ Entregable1/js/script.js
@@ -0,0 +1,18 @@
+    var imgProduct = document.getElementById("imgProduct");
+    var secImg = document.getElementsByClassName("img-sec");
+
+    secImg[0].onclick = function() {
+        imgProduct.src = secImg[0].src;
+    }
+    secImg[1].onclick = function() {
+        imgProduct.src = secImg[1].src;
+    }
+    secImg[2].onclick = function() {
+        imgProduct.src = secImg[2].src;
+    }
+    secImg[3].onclick = function() {
+        imgProduct.src = secImg[3].src;
+    }
+    secImg[4].onclick = function() {
+        imgProduct.src = secImg[4].src;
+    }
